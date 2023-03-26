8d1fe527d0d131443f611f02a299412d0e23c6eb
MrAbhishekRawat <abhishekr.rawat8@gmail.com>
2023-03-26 16:56:00 +0530

    makpagrgdynatask17

--- dynamic2.js
+++ dynamic2.js
@@ -1,53 +1,97 @@
-const posts = [];
-let lastActivityTime = null;
+// GET REQUEST
+function getTodos() {
+  console.log('GET Request');
+}
 
-async function createPost(post) {
-  return new Promise((resolve, reject) => {
-  setTimeout(() => {
-  posts.push(post);
-  resolve();
-  }, 2000);
-  });
-  }
-  
-  async function updateLastUserActivityTime() {
-  return new Promise((resolve, reject) => {
-  setTimeout(() => {
-  lastActivityTime = new Date();
-  resolve();
-  }, 1000);
-  });
-  }
-  
-  async function deleteLastPost() {
-  return new Promise((resolve, reject) => {
-  setTimeout(() => {
-  if (posts.length > 0) {
-  const deletedPost = posts.pop();
-  resolve(deletedPost);
-  } else {
-  reject("ERROR: NO POSTS FOUND");
-  }
-  }, 1500);
-  });
-  }
-  
-  function getAllPostsAndActivityTime() {
-  console.log("All posts: ", posts);
-  console.log("Last activity time: ", lastActivityTime);
-  }
-  
-  async function main() {
-  try {
-  await createPost({ title: "First Post", body: "This is my first post." });
-  await updateLastUserActivityTime();
-  getAllPostsAndActivityTime();
-  const deletedPost = await deleteLastPost();
-  console.log("Post deleted: ", deletedPost);
-  getAllPostsAndActivityTime();
-  } catch (error) {
-  console.log(error);
-  }
-  }
-  
-  main();                                               
+// POST REQUEST
+function addTodo() {
+  console.log('POST Request');
+}
+
+// PUT/PATCH REQUEST
+function updateTodo() {
+  console.log('PUT/PATCH Request');
+}
+
+// DELETE REQUEST
+function removeTodo() {
+  console.log('DELETE Request');
+}
+
+// SIMULTANEOUS DATA
+function getData() {
+  console.log('Simultaneous Request');
+}
+
+// CUSTOM HEADERS
+function customHeaders() {
+  console.log('Custom Headers');
+}
+
+// TRANSFORMING REQUESTS & RESPONSES
+function transformResponse() {
+  console.log('Transform Response');
+}
+
+// ERROR HANDLING
+function errorHandling() {
+  console.log('Error Handling');
+}
+
+// CANCEL TOKEN
+function cancelToken() {
+  console.log('Cancel Token');
+}
+
+// INTERCEPTING REQUESTS & RESPONSES
+
+// AXIOS INSTANCES
+
+// Show output in browser
+function showOutput(res) {
+  document.getElementById('res').innerHTML = `
+  <div class="card card-body mb-4">
+    <h5>Status: ${res.status}</h5>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Headers
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Data
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.data, null, 2)}</pre>
+    </div>
+  </div>
+
+  <div class="card mt-3">
+    <div class="card-header">
+      Config
+    </div>
+    <div class="card-body">
+      <pre>${JSON.stringify(res.config, null, 2)}</pre>
+    </div>
+  </div>
+`;
+}
+
+// Event listeners
+document.getElementById('get').addEventListener('click', getTodos);
+document.getElementById('post').addEventListener('click', addTodo);
+document.getElementById('update').addEventListener('click', updateTodo);
+document.getElementById('delete').addEventListener('click', removeTodo);
+document.getElementById('sim').addEventListener('click', getData);
+document.getElementById('headers').addEventListener('click', customHeaders);
+document
+  .getElementById('transform')
+  .addEventListener('click', transformResponse);
+document.getElementById('error').addEventListener('click', errorHandling);
+document.getElementById('cancel').addEventListener('click', cancelToken);
